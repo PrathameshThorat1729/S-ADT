5dae9ffe65d2e442c4e18a80673222b3613ed101
Prathamesh Thorat <thoratprathamesh1729@gmail.com>
2023-02-18 20:03:22 +0530

    Array test file edited

--- test/Array.test.cpp
+++ test/Array.test.cpp
@@ -21,25 +21,24 @@ void get_sum(int ind, int el, Array *arr, void *sum)
 
 int main()
 {
-  srand(time(0));
   int sum = 0;
   Array arr;
 
   cout << "Adding from 10 random nums ..." << endl;
   for (int i = 1; i <= 10; i++)
   {
-    arr << (rand() % 91) + 10;
+    arr << i;
   }
 
   cout << "size : " << arr.size() << ", length : " << arr.length() << endl;
   print_arr(&arr);
 
   cout << "at 4 : " << arr[4] << endl;
-  arr[4] = (rand() % 91) + 10;
+  arr[4] = 64;
   cout << "now at 4 : " << arr[4] << endl;
   print_arr(&arr);
 
-  int to_ins = (rand() % 91) + 10;
+  int to_ins = 48;
   cout << "inserting " << to_ins << " at 6 ..." << endl;
   arr.insert(6, to_ins);
   cout << "size : " << arr.size() << ", length : " << arr.length() << endl;
@@ -62,7 +61,7 @@ int main()
 
   for (int i = -5; i <= arr.size() + 5; i++)
   {
-    // cout << "[" << i << "] : " << arr.l_search(i) << " / " << arr.b_search(i, true, asc) << endl;
+    //cout << "[" << i << "] : " << arr.l_search(i) << " / " << arr.b_search(i, true, asc) << endl;
     assert(arr.l_search(i) == arr.b_search(i, true, asc));
   }
   cout << "All Searching Assertions passed" << endl;
